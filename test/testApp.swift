ff33a7c329a98927c0c7744a4ad73cdf0a26b9ee
Yala Huang <yalahuangfeng@gmail.com>
2024-03-17 22:21:01 -0400

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Teacher on 3/17/24.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
